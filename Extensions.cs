acc4693378df8d3615a8969c795ec70a41330c9e
electricessence <oren@electrified.net>
2018-10-08 20:16:33 -0700

    Initial checkin

---
+++ Extensions.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace Open.MongoDB.Extensions
+{
+    public static class Extensions
+    {
+		public static MongoClient Client(this MongoClientSettings settings)
+			=> new MongoClient(settings);
+
+		public static MongoClient Client(this MongoUrl url)
+			=> new MongoClient(url);
+	}
+}
